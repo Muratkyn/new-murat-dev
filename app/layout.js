1f170f7ea696e0702f5022b8e4672d9e31b43b24
Murat <123808058+Muratkyn@users.noreply.github.com>
2024-01-13 11:37:16 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Murat Koyun',
-  description: 'Portfolio website',
+  description: 'Murat.dev',
 }
 
 export default function RootLayout({ children }) {
